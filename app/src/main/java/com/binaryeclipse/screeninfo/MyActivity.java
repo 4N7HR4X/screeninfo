0d0d5edd1a222e95e38b9db04a7e9ff8dbea92f7
4N7HR4X <anthrax.f@gmail.com>
2014-06-30 08:08:05 +0200

    Updated display items

--- app/src/main/java/com/binaryeclipse/screeninfo/MyActivity.java
+++ app/src/main/java/com/binaryeclipse/screeninfo/MyActivity.java
@@ -39,13 +39,15 @@ public class MyActivity extends Activity {
             display.getSize(appSize);
             display.getCurrentSizeRange(smallest, largest);
             sb.append(String.format("Real Screen Resolution: %1$dx%2$d", realSize.x, realSize.y)).append('\n');
-            sb.append(String.format("Effective Resolution: %1$dx%2$d", appSize.x, appSize.y)).append('\n');
-            sb.append(String.format("Minimum Resolution: %1$dx%2$d", smallest.x, smallest.y)).append('\n');
-            sb.append(String.format("Maximum Resolution: %1$dx%2$d", largest.x, largest.y)).append('\n');
+            sb.append(String.format("App Resolution: %1$dx%2$d", appSize.x, appSize.y)).append('\n');
+            //sb.append(String.format("Minimum Resolution: %1$dx%2$d", smallest.x, smallest.y)).append('\n');
+            //sb.append(String.format("Maximum Resolution: %1$dx%2$d", largest.x, largest.y)).append('\n');
+            sb.append(String.format("App Resolution in dp: %1$dx%2$d", (int) (appSize.x/displayMetrics.scaledDensity), (int) (appSize.y/displayMetrics.scaledDensity)));
         } else {
             width = display.getWidth();
             height = display.getHeight();
             sb.append(String.format("Effective App Size: %1$dx%2$d", width, height));
+            sb.append(String.format("App Resolution in dp: %1$dx%2$d", (int) (width/displayMetrics.scaledDensity), (int) (height/displayMetrics.scaledDensity)));
         }
         TextView textView = (TextView) findViewById(R.id.dpi_label);
         textView.setTextSize(16);
